41648eaf00423195be17975cc107d3fab3935b20
TUCKO <arthuralcantara.dmf@gmail.com>
2023-10-25 21:04:55 -0300

    first codes php

---
+++ ex00/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello world! \u{1F30E}";        
+        ?>
+    </h1>
+    <h2>
+        <?php 
+        print "Houston we've a problem \u{1F680}";
+        
+        ?>
+    </h2>
+    <p>
+        tentando me livrar da maldição
+    </p>
+</body>
+</html>

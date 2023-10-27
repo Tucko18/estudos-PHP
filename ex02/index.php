98760ba039626d0719f347ac4325d1d3d475b2a0
TUCKO <arthuralcantara.dmf@gmail.com>
2023-10-26 23:18:17 -0300

    ,

--- ex02/index.php
+++ ex02/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Exemplo PHP</h1>
-    <?php 
+    <?
     //date_default_timezone_set("America/Sao_Paulo");
     echo "Hoje é dia ". date("d/M/Y"); 
     print"e a hora é" . date("G:i:s T");

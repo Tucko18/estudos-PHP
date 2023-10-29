49e11af9d810ae1c57135bed6ec151463ef7f395
TUCKO <arthuralcantara.dmf@gmail.com>
2023-10-29 17:21:36 -0300

    test

---
+++ ex03/primirtivetype.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primitive Types PHP</title>
+</head>
+<body>
+    <h1>testing Primitives Types</h1>
+    <?php 
+    // 0x = hexadecimal 0b = binario, 0 = octal
+        //$v = 300;
+        //var_dump($v);
+        //$num = (float) 3e2;
+        //echo "The value is $num";
+        //var_dump($num);
+        //$married = true;
+        //var_dump($married);
+        //print"The value is $married";
+
+       // $vet = [6, 9, "Arthur", 2.98, false];
+       //var_dump($vet);
+       class Person{
+         private string $name;
+       }
+       $p = new Person;
+       //var_dump($p);
+    ?>
+</body>
+</html>
